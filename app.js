5fd9326bea33d82d2cc7573a26fb61d69380ea7d
James McCarron <mccarron.j@gmail.com>
2021-10-22 14:35:56 +1100

    Examples of filtering admin paths

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.json());
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
