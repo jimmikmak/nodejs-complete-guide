ae84bd708bb7459fffbc447d65fc9f2d05aa84c9
James McCarron <mccarron.j@gmail.com>
2021-11-16 16:19:34 +1100

    Fetching products

--- controllers/shop.js
+++ controllers/shop.js
@@ -24,13 +24,14 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/index", {
-      prods: products,
+  Product.fetchAll().then(([rows, fieldData])=>{
+      res.render("shop/index", {
+      prods: rows,
       pageTitle: "Shop",
       path: "/",
     });
-  });
+  }).catch(err => console.log(err));
+
 };
 
 exports.getCart = (req, res, next) => {
