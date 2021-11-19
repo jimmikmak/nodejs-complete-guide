c9afba09c4c6df717b39f8f41db4d2ecabaa53d9
James McCarron <mccarron.j@gmail.com>
2021-11-19 14:14:43 +1100

    Created & fetched a cart

--- app.js
+++ app.js
@@ -46,7 +46,8 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync({ force: true })
+  // .sync({ force: true })
+  .sync()
   .then((result) => {
     return User.findByPk(1);
     // console.log(result);
@@ -59,8 +60,9 @@ sequelize
   })
   .then((user) => {
     // console.log(user);
-    app.listen(3000);
+    return user.createCart();
   })
+  .then((cart) => app.listen(3000))
   .catch((err) => {
     console.log(err);
   });
