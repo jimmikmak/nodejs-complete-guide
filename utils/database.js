57b29b7b3729aae5c9ecf74733427e351d354b49
James McCarron <mccarron.j@gmail.com>
2021-11-23 11:13:29 +1100

    Created new MongoDB database function

--- utils/database.js
+++ utils/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-complete", "root", "Taurus27", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://jimmikmak:taurus27@cluster0.a1uzt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      callback(client);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
