08d57a0bd952bd4254d32f1636862c1fa248ca0e
James McCarron <mccarron.j@gmail.com>
2021-11-24 11:18:06 +1100

    Removed cart files & added 'add to cart' functionality.

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("619d7a9bcccb414d246def4a")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => {
