b30f468c9f2760956652d80d1a4a65a7772a527b
James McCarron <mccarron.j@gmail.com>
2021-12-20 10:19:28 +1100

    Configuring cookies/typeError with split.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[0];
+  const isLoggedIn =
+    req.get("Cookie").split(";")[0].trim().split("=")[0] === "true";
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
