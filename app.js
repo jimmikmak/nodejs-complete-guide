7e3f661fe92922425702a7fb09804d5bdda26908
James McCarron <mccarron.j@gmail.com>
2021-12-20 10:32:12 +1100

    Installed and initialized session middleware.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -19,6 +20,9 @@ const authRoutes = require("./routes/auth");
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({ secret: "TOP SECRET", resave: false, saveUninitialized: false })
+);
 
 app.use((req, res, next) => {
   User.findById("61a5a184683fbfee135c7732")
