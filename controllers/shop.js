2544b4b2fdfc24123b1f1782db54be6b820994a1
James McCarron <mccarron.j@gmail.com>
2021-12-22 10:36:37 +1100

    Made add to cart work again.

--- controllers/shop.js
+++ controllers/shop.js
@@ -49,7 +49,7 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = async (req, res, next) => {
-  await req.session.user
+  await req.user
     .populate("cart.items.productId")
     .then((user) => {
       const products = user.cart.items;
@@ -69,7 +69,7 @@ exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId)
     .then((product) => {
-      return req.session.user.addToCart(product);
+      return req.user.addToCart(product);
     })
     .then((result) => {
       console.log(result);
@@ -79,7 +79,7 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  req.session.user
+  req.user
     .removeFromCart(prodId)
     .then((result) => {
       res.redirect("/cart");
@@ -90,7 +90,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = async (req, res, next) => {
-  await req.session.user
+  await req.user
     .populate("cart.items.productId")
     .then((user) => {
       console.log(user.cart.items);
@@ -99,15 +99,15 @@ exports.postOrder = async (req, res, next) => {
       });
       const order = new Order({
         user: {
-          name: req.session.user.name,
-          userId: req.session.user,
+          name: req.user.name,
+          userId: req.user,
         },
         products: products,
       });
       return order.save();
     })
     .then((result) => {
-      return req.session.user.clearCart();
+      return req.user.clearCart();
     })
     .then(() => {
       res.redirect("/orders");
