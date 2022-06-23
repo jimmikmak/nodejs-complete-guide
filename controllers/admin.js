cd385a8c860f51105209cd1a357b50965c83690f
James McCarron <mccarron.j@gmail.com>
2022-06-23 11:52:49 +1000

    Added protection to POST actions

--- controllers/admin.js
+++ controllers/admin.js
@@ -64,7 +64,7 @@ exports.postEditProduct = (req, res, next) => {
 
   Product.findById(prodId)
     .then((product) => {
-      if (product.userId !== req.user._id) {
+      if (product.userId.toString() !== req.user._id.toString()) {
         return res.redirect("/");
       }
       product.title = updatedTitle;
@@ -96,7 +96,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByIdAndRemove(prodId)
+  Product.deleteOne({ _id: prodId, userId: req.user._id })
     .then(() => {
       console.log("DESTROYED PRODUCT!");
       res.redirect("/admin/products");
