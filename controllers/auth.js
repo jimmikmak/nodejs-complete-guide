319d9712968b63dd8ad486ad611e816e190a1fe8
James McCarron <mccarron.j@gmail.com>
2022-06-27 13:07:28 +1000

    Added async validation to auth.js routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -73,7 +73,6 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors.array());
@@ -83,41 +82,29 @@ exports.postSignup = (req, res, next) => {
       errorMessage: errors.array()[0].msg,
     });
   }
-  User.findOne({ email: email })
-    .then((userDoc) => {
-      if (userDoc) {
-        req.flash(
-          "error",
-          "E-Mail already exists, please choose a different one."
-        );
-        return res.redirect("/signup");
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then((hashedPassword) => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] },
-          });
-          return user.save();
-        })
-        .then((result) => {
-          res.redirect("/login");
-          // return transporter.sendMail({
-          //   to: email,
-          //   from: "shop@node-complete.com",
-          //   subject: "Signup succeeded!",
-          //   html: "<h1>You successfully signed up!</h1>",
-          // });
-        });
-      // .catch((err) => {
-      //   console.log(err);
-      // });
+
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] },
+      });
+      return user.save();
     })
-    .catch((err) => {
-      console.log(err);
+    .then((result) => {
+      res.redirect("/login");
+      // return transporter.sendMail({
+      //   to: email,
+      //   from: "shop@node-complete.com",
+      //   subject: "Signup succeeded!",
+      //   html: "<h1>You successfully signed up!</h1>",
+      // });
     });
+  // .catch((err) => {
+  //   console.log(err);
+  // });
 };
 
 exports.postLogout = (req, res, next) => {
