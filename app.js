880d3e3a48fa2b88e156332d83f003e07a3d3221
James McCarron <mccarron.j@gmail.com>
2022-07-19 12:55:06 +1000

    Fixed 500 error logic in app.js

--- app.js
+++ app.js
@@ -102,7 +102,12 @@ app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
   // res.status(error.httpStatusCode).render(...);
-  res.redirect("/500");
+  // res.redirect('/500');
+  res.status(500).render("500", {
+    pageTitle: "Error!",
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn,
+  });
 });
 
 mongoose
